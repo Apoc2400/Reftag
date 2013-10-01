aab0a751363d68129e020a37c8935396e2488df8
Apoc2400 <apoc2400@gmail.com>
2013-10-01 21:27:04 +0200

    First attempt to save settings in cookies.

--- static/gb.js
+++ static/gb.js
@@ -93,6 +93,8 @@ function splitName(i) {
 }
 
 function makeCiteBook() {
+  saveCookies();
+
   var cite = '<ref';
   var refname = document.getElementById('refname').value;
   if (/\S/.test(refname)) {
@@ -429,6 +431,7 @@ function loadRun() {
     makeRefname();
     setAccessDateToday();
     checkAuthorLinks();
+    readCookies();
     makeCiteBook();
     makeCopyButton()
   }
@@ -437,3 +440,39 @@ function loadRun() {
   }
 }
 
+function setCookie(c_name,value,exdays) {
+  var exdate=new Date();
+  exdate.setDate(exdate.getDate() + exdays);
+  var c_value=escape(value) + 
+    ((exdays==null) ? "" : ("; expires="+exdate.toUTCString()));
+  document.cookie=c_name + "=" + c_value;
+}
+
+function getCookie(c_name) {
+  var i,x,y,ARRcookies=document.cookie.split(";");
+  for (i=0;i<ARRcookies.length;i++)
+  {
+    x=ARRcookies[i].substr(0,ARRcookies[i].indexOf("="));
+    y=ARRcookies[i].substr(ARRcookies[i].indexOf("=")+1);
+    x=x.replace(/^\s+|\s+$/g,"");
+    if (x==c_name)
+    {
+      return unescape(y);
+    }
+  }
+}
+
+function saveCookies() {
+  var harv = document.getElementById('harv').checked;
+  setCookie("harv", harv, 1);
+  
+  var extraparams = document.getElementById('extraparams').checked;
+  setCookie("extraparams", extraparams, 1);
+
+
+}
+
+function readCookies() {
+  document.getElementById('harv').checked = getCookie("harv") == "true";
+  document.getElementById('extraparams').checked = getCookie("extraparams") == "true";
+}
