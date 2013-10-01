ed1fdf0cc2fdb8153d3be7f11297409a84db1caf
Apoc2400 <apoc2400@gmail.com>
2013-10-01 19:56:45 +0200

    test_all.sh now starts the dev appserver.

--- test/test_all.sh
+++ test/test_all.sh
@@ -1,3 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+cd $(dirname "$0")
 
+echo "Starting the dev appserver..."
+dev_appserver.py .. &> appserver.log &
+sleep 3
+
+echo "Running the test..."
 python -m unittest test_with_browser test_jsonp_api
+
+echo "Stopping the dev appserver..."
+kill $!
