e70ad0f2a3efe3605d5242d949a40ce097929214
Apoc2400 <apoc2400@gmail.com>
2013-10-06 13:27:37 +0200

    More functionality for OCLC.

--- test/test_oclc.py
+++ test/test_oclc.py
@@ -1,9 +1,28 @@
 import unittest
-from worldcat_api import get_by_oclc
+from worldcat_api import get_by_oclc, get_by_isbn
 
 class TestOclc(unittest.TestCase):
     def test_oclc(self):
-        oclc = 550538756
+        oclc = '550538756'
         info = get_by_oclc(oclc)
         self.assertEquals(info['title'], "On hobos and homelessness")
+        self.assertIn('Nels Anderson', info['author'])
+        self.assertIn('1998', info['date'])
+        self.assertIn('Chicago Press', info['publishers'][0])
 
+        identifiers = info['identifiers']
+        self.assertIn(('oclc', oclc), identifiers)
+        self.assertIn(('isbn', '9780226019666'), identifiers)
+
+    def test_isbn(self):
+        isbn = '1858286999'
+        info = get_by_isbn(isbn)
+        self.assertIn('Japan', info['title'])
+        self.assertIn('Jan Dodd', info['author'])
+        self.assertIn('Simon Richmond', info['author'])
+        self.assertIn('2001', info['date'])
+        self.assertIn('Rough Guides', info['publishers'][0])
+
+        identifiers = info['identifiers']
+        self.assertIn(('oclc', '222891799'), identifiers)
+        self.assertIn(('isbn', isbn), identifiers)
--- worldcat_api.py
+++ worldcat_api.py
@@ -1,26 +1,53 @@
 from worldcat.request.xid import xOCLCNUMRequest, xISBNRequest
 
 def get_by_oclc(oclc):
-
     o = xOCLCNUMRequest(rec_num=oclc, method='getMetadata').get_response().data
 
     for i in o['list']:
         #print i
         for isbn in i['isbn']:
             #print isbn
-            return get_by_isbn(isbn)
+            output = get_by_isbn(isbn)
+            # First the known oclc, in case it is not returned from WorldCat
+            output['identifiers'].add(('oclc', str(oclc)))
+            return output
 
 def get_by_isbn(isbn):
-    output = {}
     o = xISBNRequest(rec_num=isbn, method='getMetadata').get_response().data
-    for i in o['list']:
-        #print i
-        if 'title' in i:
-            output['title'] = i['title']
+    
+    assert len(o['list']) == 1
+    data_dict = o['list'][0]
+    print data_dict
+
+    output = {}
+
+    fields = ['title', 'author']
+    for field in fields:
+        if field in data_dict:
+            output[field] = data_dict[field]
+            
+    if 'year' in data_dict:
+        output['date'] = data_dict['year']
+            
+    if 'publisher' in data_dict:
+        output['publishers'] = [data_dict['publisher']]
+            
+    identifiers = set()
+    # First add the known isbn
+    identifiers.add(('isbn', str(isbn)))
+    if 'isbn' in data_dict:
+        for otherisbn in data_dict['isbn']:
+            identifiers.add(('isbn', otherisbn))
+    if 'oclcnum' in data_dict:
+        for otheroclc in data_dict['oclcnum']:
+            identifiers.add(('oclc', otheroclc))
+    output['identifiers'] = identifiers
 
     return output
 
 
 if __name__ == "__main__":
     print get_by_oclc(550538756)
+    #print get_by_oclc(222891799)
+    
     
